b370ec1734a125443268ebe41ffbf81ac738e4f0
CarlosG18 <c.gabriel.abc18@gmail.com>
2022-12-18 17:29:41 -0300

    att

--- components/decrementador12.vhdl
+++ components/decrementador12.vhdl
@@ -13,10 +13,9 @@ END decrementador12;
 ARCHITECTURE comportamento OF decrementador12 IS
 BEGIN
   PROCESS (data_decre, Clock_decre)
-  VARIABLE qv : INTEGER RANGE 3599 DOWNTO 0;
+  VARIABLE qv : INTEGER RANGE 3599 DOWNTO 0 := data_decre;
   BEGIN
     IF Clock_decre 'EVENT AND Clock_decre = '1' THEN
-      qv := data_decre;
       qv := qv - 1;
     END IF;
     IF Clock_decre 'EVENT AND Clock_decre = '1' AND qv = 0 THEN
