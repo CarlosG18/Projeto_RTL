c70b7baacca0d4213a57953b2d36ac487177c4d4
CarlosG18 <c.gabriel.abc18@gmail.com>
2022-12-18 20:52:10 -0300

    add

--- components/decrementador12.vhdl
+++ components/decrementador12.vhdl
@@ -3,18 +3,21 @@ USE ieee.std_logic_1164.all ;
 
 ENTITY decrementador12 IS
   PORT(
-    data_decre : IN INTEGER RANGE 3599 DOWNTO 0;
+   -- data_decre : IN INTEGER RANGE 3599 DOWNTO 0;
+  data_decre : IN STD_LOGIC_VECTOR(11 downto 0) ;
     Clock_decre : IN BIT;
     tc : OUT BIT;
     load : IN BIT;
-    Q_decre : OUT INTEGER RANGE 3599 DOWNTO 0
+    Q_decre : OUT STD_LOGIC_VECTOR(11 downto 0)
+    --Q_decre : OUT INTEGER RANGE 3599 DOWNTO 0
   );
 END decrementador12;
 
 ARCHITECTURE comportamento OF decrementador12 IS
 BEGIN
   PROCESS (data_decre, Clock_decre, load)
-  VARIABLE qv : INTEGER RANGE 3599 DOWNTO 0;
+  --VARIABLE qv : INTEGER RANGE 3599 DOWNTO 0;
+  VARIABLE qv : STD_LOGIC_VECTOR(11 downto 0);
   BEGIN
 
     IF Clock_decre 'EVENT AND Clock_decre = '1' THEN
