14476e4adbbed112f0a56c038c9c83226eb8122b
CarlosG18 <c.gabriel.abc18@gmail.com>
2022-12-19 18:16:42 -0300

    edit main

--- components/comparador12.vhdl
+++ components/comparador12.vhdl
@@ -3,7 +3,7 @@ USE ieee.std_logic_1164.all ;
 
 ENTITY comparador12 IS
   PORT ( 
-  data_comp : IN STD_LOGIC_VECTOR(11 downto 0) ;
+  data_comp : IN INTEGER RANGE 3599 DOWNTO 0;
   RegTemp_END_comp : OUT BIT
   ) ;
 END comparador12 ;
@@ -12,7 +12,7 @@ ARCHITECTURE Behavior OF comparador12 IS
 BEGIN
   PROCESS ( data_comp )
   BEGIN
-    IF data_comp = "000000000000" THEN
+    IF data_comp = 0 THEN
       RegTemp_END_comp  <= '1';
     ELSE
       RegTemp_END_comp <= '0';
