189c2665ae027a64f3f76225fcf371bea1b34d0a
CarlosG18 <c.gabriel.abc18@gmail.com>
2022-12-20 16:01:09 -0300

    att main

--- components/decrementador12.vhdl
+++ components/decrementador12.vhdl
@@ -14,13 +14,17 @@ ENTITY decrementador12 IS
 END decrementador12;
 
 ARCHITECTURE comportamento OF decrementador12 IS
+signal load_aux : bit;
 BEGIN
-  PROCESS (data_decre, Clock_decre, load)
+  load_aux <= load;
+
+  PROCESS (data_decre, Clock_decre, load_aux)
   VARIABLE qv : INTEGER RANGE 3599 DOWNTO 0;
   BEGIN
     IF Clock_decre 'EVENT AND Clock_decre = '1' THEN
-	IF load = '1' THEN
+	IF load_aux = '1' THEN
 	  qv := data_decre;
+	  --load_aux <= '0';
 	ELSE 
 	  IF (qv > 0) THEN 
 	    qv := qv - 1;
