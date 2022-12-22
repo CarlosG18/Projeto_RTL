43f990a01bd78ab5a275b85b317be0740ed33194
CarlosG18 <c.gabriel.abc18@gmail.com>
2022-12-22 15:10:12 -0300

    att main

--- main_.vhdl
+++ main_.vhdl
@@ -23,8 +23,6 @@ architecture comportamento of main is
 
     signal estado : tipoestado;
     signal reg_showtime : unsigned (12 downto 0);
-    signal reg_estados :unsigned (2 downto 0);
-    signal reg_decre : unsigned (12 downto 0);
     signal tc : std_logic;
     signal b1_aux : std_logic;
     signal b2_aux : std_logic;
@@ -34,12 +32,6 @@ architecture comportamento of main is
     constant u_um : unsigned (0 downto 0) := "1";
 begin
 
-  OnMicro <= not(b1_aux) and b2_aux and b3_aux;
-  OnAlarm <=(b1_aux and not(b2_aux) and not(b3_aux) and not(CDoor)) or (b1_aux and not(b2_aux) and not(b3_aux) and CDoor);
-  --RegTemp_L <= (not(b1_aux) and not(b2_aux) and b3_aux and not(BtnTime)) or (not(b1_aux) and not(b2_aux) and b3_aux and BtnTime);
-  --regtempLoad <=(not(b1_aux) and not(b2) and b3_aux and not(BtnTime)) or (not(b1_aux) and not(b2_aux) and b3_aux and BtnTime);
-  --RegTemp_C <= b1_aux and not(b2_aux) and not(b3_aux);
-  --regtempClear <= b1_aux and not(b2_aux) and not(b3_aux);
   n1 <= (not(b1_aux) and b2_aux and b3_aux and regtemp_end_aux) and (b1_aux and not(b2_aux) and not(b3_aux) and not(CDoor));
   n2 <= (not(b1_aux) and not(b2_aux) and b3_aux and BtnTime) or (not(b1_aux) and b2_aux and not(b3_aux)) or (not(b1_aux) and b2_aux and b3_aux and not(tc));
   n3 <= (not(b1_aux) and not(b2_aux) and not(b3_aux)) or (not(b1_aux) and b2_aux and not(b3_aux) and BtnOn and CDoor) or (not(b1_aux) and b2_aux and b3_aux and not(tc)) or (b1_aux and not(b2_aux) and not(b3_aux) and CDoor);
@@ -54,9 +46,9 @@ begin
         elsif (clk='1' and clk'event) then
             case estado is
                 when START =>
-                    --onMicro <= '0';
-                    --onAlarm <= '0';
-                    reg_estados <= '000';
+                    onMicro <= '0';
+                    onAlarm <= '0';
+                    reg_estados <= "000";
                     b1_aux <= '0';
                     b2_aux <= '0';
                     b3_aux <= '0';
@@ -69,7 +61,7 @@ begin
                         estado <= START;
                     end if;
                 when SETS =>
-                    reg_estados <= '001';
+                    reg_estados <= "001";
                     b1_aux <= '0';
                     b2_aux <= '0';
                     b3_aux <= '1';
@@ -77,10 +69,9 @@ begin
                     b2 <= '0';
                     b3 <= '1';
                     reg_showtime <= Data_time;
-                    show_time <= reg_showtime;
                     estado <= ESPERA;
                 when ESPERA =>
-                    reg_estados <= '010';
+                    reg_estados <= "010";
                     b1_aux <= '0';
                     b2_aux <= '1';
                     b3_aux <= '0';
@@ -93,7 +84,7 @@ begin
                         estado <= ESPERA;
                     end if;
                 when ONMICRO1 =>
-                    reg_estados <= '011';
+                    reg_estados <= "011";
                     b1_aux <= '0';
                     b2_aux <= '1';
                     b3_aux <= '1';
@@ -102,7 +93,7 @@ begin
                     b3 <= '1';
                     reg_showtime <= reg_showtime - 1;
                     show_time <= reg_showtime;
-                    --onMicro <= '1';
+                    onMicro <= '1';
                     if (reg_showtime = u_zero) then 
                         tc <= '1';
                         estado <= ONALARM1;
@@ -111,15 +102,15 @@ begin
                         estado <= ONMICRO1;
                     end if;
                 when ONALARM1 =>
-                    reg_estados <= '100';
+                    reg_estados <= "100";
                     b1_aux <= '1';
                     b2_aux <= '0';
                     b3_aux <= '0';
                     b1 <= '1';
                     b2 <= '0';
                     b3 <= '0';
-                    --onAlarm <= '1';
-                    --onMicro <= '0';
+                    onAlarm <= '1';
+                    onMicro <= '0';
                     reg_showtime <= u_zero;
                     if (CDoor='1') then 
                         estado <= ONALARM1;
